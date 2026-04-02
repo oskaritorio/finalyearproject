594ca87449f56a82e56ba7a62d7366a1c5426f0a
oskaritorio <oskarkandulski@gmail.com>
2026-04-02 13:16:06 +0100

    fixing errors and docker isssues

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,21 +1,27 @@
-
 from sqlalchemy.orm import relationship
 from .base import Base, engine, get_db
 from .user import User
 from .journal import JournalEntry
 from .mood import MoodLog
+from .chat import ChatSession, Message
 
-# Set up relationships
+#Journal
 User.journal_entries = relationship("JournalEntry", back_populates="user", cascade="all, delete-orphan")
 JournalEntry.user = relationship("User", back_populates="journal_entries")
-User.mood_logs = relationship("MoodLog", back_populates="user", cascade= "all, delete-orphan")
+
+#Mood
+User.mood_logs = relationship("MoodLog", back_populates="user", cascade="all, delete-orphan")
 MoodLog.user = relationship("User", back_populates="mood_logs")
-User.chat_sessions = relationship("Chatsession", back_populates="user", cascade="all, delete_orphan")
+
+#User chat
+User.chat_sessions = relationship("ChatSession", back_populates="user", cascade="all, delete-orphan")
 ChatSession.user = relationship("User", back_populates="chat_sessions")
+
+#Message
 ChatSession.messages = relationship("Message", back_populates="session", cascade="all, delete-orphan")
 Message.session = relationship("ChatSession", back_populates="messages")
 
-# Export all models and utilities
+#Export all models and utilities
 __all__ = [
     "Base", 
     "engine", 
--- backend/app/models/chat.py
+++ backend/app/models/chat.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Column, String, Integer, DataTime, ForeignKey, Boolean, Text
-from datatime import datetime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Boolean, Text
+from datetime import datetime
 import uuid
 from .base import Base
 
-class Chatsession(Base):
+class ChatSession(Base):
     __tablename__ = "chat_sessions"
 
-    id = Column(String(24), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     user_id = Column(String(36), ForeignKey("users.id"), nullable=False)
-    started_at = Column(DateTime, default=datetime.gmtnow)
+    started_at = Column(DateTime, default=datetime.utcnow)
     
     #Track if crisis happened in this chat
     crisis_detected = Column(Boolean, default=False)
@@ -21,4 +21,4 @@ class Message(Base):
     session_id = Column(String(36), ForeignKey("chat_sessions.id"), nullable=False)
     sender = Column(String(10), nullable=False)
     content = Column(Text, nullable=False)
-    timestamp = Column(DateTime, default=datetime.gmtnow)
+    timestamp = Column(DateTime, default=datetime.utcnow)
