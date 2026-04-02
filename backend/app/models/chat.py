62cd7d96335fb9123c9989a4a5614b6bf630203c
oskaritorio <oskarkandulski@gmail.com>
2026-04-02 12:28:16 +0100

    Basic code for the interaction between the AI and user

---
+++ backend/app/models/chat.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, String, Integer, DataTime, ForeignKey, Boolean, Text
+from datatime import datetime
+import uuid
+from .base import Base
+
+class Chatsession(Base):
+    __tablename__ = "chat_sessions"
+
+    id = Column(String(24), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(String(36), ForeignKey("users.id"), nullable=False)
+    started_at = Column(DateTime, default=datetime.gmtnow)
+    
+    #Track if crisis happened in this chat
+    crisis_detected = Column(Boolean, default=False)
+
+#Individual messages in a chat
+class Message(Base):
+    __tablename__ = "messages"
+    
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    session_id = Column(String(36), ForeignKey("chat_sessions.id"), nullable=False)
+    sender = Column(String(10), nullable=False)
+    content = Column(Text, nullable=False)
+    timestamp = Column(DateTime, default=datetime.gmtnow)
