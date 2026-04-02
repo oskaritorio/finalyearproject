43da061e0d1ea701a1d8d63259c16755950b2f67
oskaritorio <oskarkandulski@gmail.com>
2026-04-02 12:33:01 +0100

    Added relationships with parent and child for a working environment

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -10,6 +10,10 @@ User.journal_entries = relationship("JournalEntry", back_populates="user", casca
 JournalEntry.user = relationship("User", back_populates="journal_entries")
 User.mood_logs = relationship("MoodLog", back_populates="user", cascade= "all, delete-orphan")
 MoodLog.user = relationship("User", back_populates="mood_logs")
+User.chat_sessions = relationship("Chatsession", back_populates="user", cascade="all, delete_orphan")
+ChatSession.user = relationship("User", back_populates="chat_sessions")
+ChatSession.messages = relationship("Message", back_populates="session", cascade="all, delete-orphan")
+Message.session = relationship("ChatSession", back_populates="messages")
 
 # Export all models and utilities
 __all__ = [
@@ -18,5 +22,7 @@ __all__ = [
     "get_db", 
     "User", 
     "JournalEntry",
-    "MoodLog"
+    "MoodLog",
+    "ChatSession",
+    "Message"
 ]
